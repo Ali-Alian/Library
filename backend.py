5da246a58d332f6c50678537eff0798a8bdd65d6
Ali-Alian <alimansuralian@gmail.com>
2020-08-11 23:06:29 +0800

    upgrade the backend to OOP

--- backend.py
+++ backend.py
@@ -1,65 +1,56 @@
 import sqlite3
 
 
+class Database:
 
-def connect():
+	def __init__(self, db):
 
-	conn=sqlite3.connect('library.db')
-	cur=conn.cursor()
-	cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
-	conn.commit()
-	conn.close()
-
-
-def insert(title,author,year,isbn):
-	conn=sqlite3.connect('library.db')
-	cur=conn.cursor()
-	cur.execute("INSERT INTO books VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
-	conn.commit()
-	conn.close()
+		self.conn=sqlite3.connect(db)
+		self.cur=self.conn.cursor()
+		self.cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+		self.conn.commit()
+		
 
 
-def view():
-	conn=sqlite3.connect('library.db')
-	cur=conn.cursor()
-	cur.execute("SELECT * FROM books")
-	rows=cur.fetchall()
-	conn.close()
-	return rows
+	def insert(self,title,author,year,isbn):
+		
+		self.cur.execute("INSERT INTO books VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
+		self.conn.commit()
+		#self.conn.close()
+
+
+	def view(self):
+		
+		self.cur.execute("SELECT * FROM books")
+		rows=self.cur.fetchall()
+		#self.conn.close()
+		return rows
+	
+
+	def serching(self,title="",author="",year="",isbn=""):
+		
+		self.cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+		rows=self.cur.fetchall()
+		#conn.close()
+		return rows
+
+
+	def delete(self,id):
+
+		self.cur.execute("DELETE FROM books WHERE id=?",(id,))	# deleteing the data 
+		rows=self.cur.fetchall()
+		self.conn.commit()
+
+
+
+	def update(self,id,title,author,year,isbn):
+
+		self.cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))	#updating (price and quantitys) the inserted data 
+		rows=self.cur.fetchall()
+		self.conn.commit()
+		#conn.close()
+
+	def __del__(self):
+		self.conn.close()
+
  
-
-def serching(title="",author="",year="",isbn=""):
-	conn=sqlite3.connect("library.db")
-	cur=conn.cursor()
-	cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
-	rows=cur.fetchall()
-	conn.close()
-	return rows
-
-
-def delete(id):
-
-	conn = sqlite3.connect("library.db")
-	cur= conn.cursor()
-	cur.execute("DELETE FROM books WHERE id=?",(id,))	# deleteing the data 
-	rows=cur.fetchall()
-	conn.commit()
-	conn.close()
-
-
-def update(id,title,author,year,isbn):
-
-	conn = sqlite3.connect("library.db")
-	cur= conn.cursor()
-	cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))	#updating (price and quantitys) the inserted data 
-	rows=cur.fetchall()
-	conn.commit()
-	conn.close()
-
-
-connect()
-#insert("kirkhar","mamad",1998,23456789)
-#delete(3)
-#update(3,"bi adab",'asdf',1234,1234)
-#print(view())
-#print(serching(author="mamad"))
